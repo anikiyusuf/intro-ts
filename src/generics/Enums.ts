1b20aef903f3f3937a75ac10858b5b5de2590687
Aniki Yusuf <anikiyusuf99@gmail.com>
2023-07-04 14:55:48 +0100

    initial commit

--- src/generics/Enums.ts
+++ src/generics/Enums.ts
@@ -10,13 +10,13 @@ interface Resource <T> {
 
 const docFive: Resource<object> = {
     uid:1,
-    resourceName: ResourceType.Book,
+    resourceName: ResourceType.Book, 
     data: { title: "Things fall"}
 }
 
 const docSix: Resource <object> = {
     uid: 2,
-    resourceName: ResourceType.Film,
+    resourceName: ResourceType.Film, 
     data:{name:'Yusuf'}
 
 }
